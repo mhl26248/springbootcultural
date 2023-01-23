1227fb30d0bb566df53e1cad533358a1d2d81800
aceyang <ace.yang@softtek.com>
2023-01-23 15:54:16 +0800

    聊天

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -3,7 +3,7 @@
     <div class="video-container">
       <div :style="bg"  class="filter">
         <div style="width: 400px; margin: 100px auto">
-          <div style="font-size: 30px; text-align: center; padding: 30px 0; color: #333">旅游平台</div>
+          <div style="font-size: 30px; text-align: center; padding: 30px 0; color: #333">去旅游</div>
           <el-form ref="form" :model="form" size="normal" :rules="rules">
             <el-form-item prop="username">
               <el-input prefix-icon="el-icon-user-solid" v-model="form.username" placeholder="请输入账号"></el-input>
--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,5 +1,5 @@
 
-const name =  "旅游平台"; // page title
+const name =  "去旅游"; // page title
 
 
 // 跨域配置
